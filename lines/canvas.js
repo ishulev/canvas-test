0ad318bc4a1513f865ff49ac46d8484b529bbe66
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2017-11-08 21:12:54 +0200

    init for lines

---
+++ lines/canvas.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('canvas');
+const canvWidh = canvas.width = window.innerWidth;
+const canvHeight = canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+
+(function drawLines() {
+    console.log(ctx);
+})();
