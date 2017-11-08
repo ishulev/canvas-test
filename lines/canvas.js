b9c3850ccfde55673f6cf9c79b307f3fd594a590
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2017-11-08 21:53:50 +0200

    square sketch

--- lines/canvas.js
+++ lines/canvas.js
@@ -1,8 +1,63 @@
 const canvas = document.querySelector('canvas');
-const canvWidh = canvas.width = window.innerWidth;
+const canvWidth = canvas.width = window.innerWidth;
 const canvHeight = canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
+const initialPoint = [canvWidth - 100, 50];
+let lastPoint;
 
-(function drawLines() {
-    console.log(ctx);
-})();
+ctx.beginPath();
+
+ctx.moveTo.apply(ctx, initialPoint);
+(function drawRightLines(iterator) {
+    const x = canvWidth - 100 + Math.random() * 50;
+    const y = 50 + iterator;
+    ctx.lineTo(x, y);
+    if(iterator < 500) {
+        drawRightLines(iterator + 10);
+    }
+    else {
+        lastPoint = {
+            x: x,
+            y: y
+        }
+    }
+})(10);
+(function drawBottomLines(iterator) {
+    const x = lastPoint.x - iterator;
+    const y = lastPoint.y - Math.random() * 50;
+    ctx.lineTo(x, y);
+    if(iterator < 500) {
+        drawBottomLines(iterator + 10);
+    }
+    else {
+        lastPoint = {
+            x: x,
+            y: y
+        }
+    }
+})(10);
+(function drawLeftLines(iterator) {
+    const x = lastPoint.x - Math.random() * 50;
+    const y = lastPoint.y - iterator;
+    ctx.lineTo(x, y);
+    if(iterator < 500) {
+        drawLeftLines(iterator + 10);
+    }
+    else {
+        lastPoint = {
+            x: x,
+            y: y
+        }
+    }
+})(10);
+(function drawTopLines(iterator) {
+    const x = lastPoint.x + iterator;
+    const y = lastPoint.y + Math.random() * 50;
+    ctx.lineTo(x, y);
+    if(iterator < 490) {
+        drawTopLines(iterator + 10);
+    }
+})(10);
+// Connect last to first dot
+ctx.lineTo.apply(ctx, initialPoint);
+ctx.stroke();
