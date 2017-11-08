f8b33b4e4ff6aef0e8d6f315341cc75eb57ccb26
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2017-11-08 23:16:53 +0200

    wip

--- lines/canvas.js
+++ lines/canvas.js
@@ -3,21 +3,38 @@ const canvWidth = canvas.width = window.innerWidth;
 const canvHeight = canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 const initialPoint = [canvWidth - 100 + Math.random() * 10, 50 + Math.random() * 10];
+const pointHolder = {
+    right: []
+};
 let lastPoint;
+
 
 function randomizeLineColor() {
     ctx.strokeStyle = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
     // ctx.strokeStyle = 'black';
 }
 
-function drawCircle() {
+function drawSquare() {
+    const movement = Math.random() * 5;
     ctx.clearRect(0, 0, canvWidth, canvHeight);
     ctx.beginPath();
     ctx.moveTo.apply(ctx, initialPoint);
     (function drawRightLines(iterator) {
-        randomizeLineColor();
-        const x = canvWidth - 100 + Math.random() * 10;
-        const y = 50 + iterator;
+        // randomizeLineColor();
+        let x, y;
+        if (pointHolder.right[iterator]) {
+            console.log('here');
+            x = pointHolder.right[iterator][0];
+            y = pointHolder.right[iterator][1];
+            pointHolder.right[iterator]= [x, y];
+        }
+        else {
+            const currentPointLocation = {};
+            x = canvWidth - 100 + Math.random() * 10;
+            y = 50 + iterator;
+            currentPointLocation[iterator] = [x, y];
+            pointHolder.right.push(currentPointLocation);
+        }
         ctx.lineTo(x, y);
         if (iterator < 500) {
             drawRightLines(iterator + 10);
@@ -29,49 +46,49 @@ function drawCircle() {
             }
         }
     })(10);
-    (function drawBottomLines(iterator) {
-        randomizeLineColor();
-        const x = lastPoint.x - iterator;
-        const y = lastPoint.y - Math.random() * 10;
-        ctx.lineTo(x, y);
-        if (iterator < 500) {
-            drawBottomLines(iterator + 10);
-        }
-        else {
-            lastPoint = {
-                x: x,
-                y: y
-            }
-        }
-    })(10);
-    (function drawLeftLines(iterator) {
-        randomizeLineColor();
-        const x = lastPoint.x - Math.random() * 10;
-        const y = lastPoint.y - iterator;
-        ctx.lineTo(x, y);
-        if (iterator < 490) {
-            drawLeftLines(iterator + 10);
-        }
-        else {
-            lastPoint = {
-                x: x,
-                y: y
-            }
-        }
-    })(10);
-    (function drawTopLines(iterator) {
-        randomizeLineColor();
-        const x = lastPoint.x + iterator;
-        const y = lastPoint.y - Math.random() * 10;
-        ctx.lineTo(x, y);
-        if (iterator < 490) {
-            drawTopLines(iterator + 10);
-        }
-    })(10);
+    // (function drawBottomLines(iterator) {
+    //     randomizeLineColor();
+    //     const x = lastPoint.x - iterator;
+    //     const y = lastPoint.y - Math.random() * 10;
+    //     ctx.lineTo(x, y);
+    //     if (iterator < 500) {
+    //         drawBottomLines(iterator + 10);
+    //     }
+    //     else {
+    //         lastPoint = {
+    //             x: x,
+    //             y: y
+    //         }
+    //     }
+    // })(10);
+    // (function drawLeftLines(iterator) {
+    //     randomizeLineColor();
+    //     const x = lastPoint.x - Math.random() * 10;
+    //     const y = lastPoint.y - iterator;
+    //     ctx.lineTo(x, y);
+    //     if (iterator < 490) {
+    //         drawLeftLines(iterator + 10);
+    //     }
+    //     else {
+    //         lastPoint = {
+    //             x: x,
+    //             y: y
+    //         }
+    //     }
+    // })(10);
+    // (function drawTopLines(iterator) {
+    //     randomizeLineColor();
+    //     const x = lastPoint.x + iterator;
+    //     const y = lastPoint.y - Math.random() * 10;
+    //     ctx.lineTo(x, y);
+    //     if (iterator < 490) {
+    //         drawTopLines(iterator + 10);
+    //     }
+    // })(10);
     // Connect last to first dot
     ctx.lineTo.apply(ctx, initialPoint);
     ctx.stroke();
-    window.requestAnimationFrame(drawCircle);
+    window.requestAnimationFrame(drawSquare);
 }
-
-window.requestAnimationFrame(drawCircle);
+window.requestAnimationFrame(drawSquare);
+console.log(pointHolder);
